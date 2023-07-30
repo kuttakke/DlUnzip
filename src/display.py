ecbc5f1c30bcef999f17d0b2706ff602afd1a79e
KuTaKe <1449568043@qq.com>
2023-07-30 14:43:38 +0800

    ✨  use rich & rewrite

---
+++ src/display.py
@@ -0,0 +1,133 @@
+﻿from contextlib import contextmanager
+from datetime import datetime
+from enum import Enum
+
+from rich.console import Console
+from rich.layout import Layout
+from rich.live import Live
+from rich.panel import Panel
+from rich.prompt import Confirm, Prompt
+from rich.table import Table
+from rich.text import Text
+from rich.traceback import install
+
+
+# 要更新的layout位置
+class LayoutName(Enum):
+    FILES = "left"
+    LOGGER = "right_top"
+    PROCESS = "right_bottom"
+    NOW_FILE = "now_file"
+
+
+class make_header:
+    """Display header with clock."""
+
+    def __rich__(self) -> Panel:
+        grid = Table.grid(expand=True)
+        grid.add_column(justify="center", ratio=1)
+        grid.add_column(justify="right")
+        grid.add_row(
+            "[b]DlUnzip[/b]",
+            datetime.now().ctime().replace(":", "[blink]:[/]"),
+        )
+        return Panel(grid, style="red on black")
+
+
+class Display:
+    console = Console(height=25)
+
+    install(console=console, show_locals=True)
+
+    layout = Layout(name="root")
+    layout_hander = Layout(name="hander", size=3)
+    default_logger_table = Table(
+        expand=True,
+        show_header=False,
+        header_style="bold",
+        title="[blue][b]Log Messages[/b]",
+        highlight=True,
+    )
+    default_logger_table.add_column("Log Output")
+    layout_logger = Layout(Panel(default_logger_table), name="logger", ratio=2)
+    layout_files = Layout(name="files")
+    layout_now_file = Layout(
+        Panel(
+            Text(" - ", justify="center", overflow="ellipsis"), border_style="magenta"
+        ),
+        name="now_file",
+    )
+    layout_process = Layout(name="process")
+
+    layout.split(layout_hander, Layout(name="main"))
+    layout["main"].split_row(layout_files, Layout(name="right"))
+    # layout["right"].split_column(layout_logger, layout_process)
+    layout["right"].split_column(layout_logger, Layout(name="right_bottom"))
+    layout["right_bottom"].split_column(layout_now_file, layout_process)
+
+    _layout_map = {
+        LayoutName.FILES: layout_files,
+        LayoutName.LOGGER: layout_logger,
+        LayoutName.PROCESS: layout_process,
+        LayoutName.NOW_FILE: layout_now_file,
+    }
+
+    _now_live: Live
+
+    @classmethod
+    def layout_init(cls):
+        cls.layout_hander.update(make_header())
+        # cls.layout_logger.update(Panel(cls.default_logger_table, border_style="red"))
+        cls.layout_process.update(Panel("process", border_style="magenta"))
+
+    # live context
+    @classmethod
+    @contextmanager
+    def live(cls):
+        """动态显示"""
+
+        with Live(
+            cls.layout, console=cls.console, refresh_per_second=10, screen=True
+        ) as live:
+            cls._now_live = live
+            yield live
+
+    @classmethod
+    def _make_defalut_logger_table(cls) -> Table:
+        """制作默认的logger table"""
+        table = Table(
+            expand=True,
+            show_header=False,
+            header_style="bold",
+            title="[blue][b]Log Messages[/b]",
+            highlight=True,
+        )
+        table.add_column("Log Output")
+        return table
+
+    @classmethod
+    def redraw_logger_table(cls, log_list: list[str]) -> Table:
+        """重绘logger table"""
+        table = cls._make_defalut_logger_table()
+        for log in log_list:
+            table.add_row(log)
+        return table
+
+    @classmethod
+    def display(cls, name: LayoutName, panel: Panel):
+        """更新layout"""
+        cls._layout_map[name].update(panel)
+
+    @classmethod
+    def ask_for_password(cls, filename: str) -> str:
+        cls._now_live.stop()
+        password = Prompt.ask(f"请为{filename}输入密码\n")
+        cls._now_live.start()
+        return password
+
+    @classmethod
+    def is_skip(cls, filename: str) -> bool:
+        cls._now_live.stop()
+        skip = Confirm.ask(f"是否跳过{filename}？否则停止程序")
+        cls._now_live.start()
+        return skip
---
+++ src/rich_log.py
@@ -0,0 +1,125 @@
+﻿import logging
+import os
+from typing import Callable
+
+from loguru import logger
+from loguru._logger import Logger
+from rich.layout import Layout
+from rich.panel import Panel
+from rich.table import Table
+
+_color_map = {
+    "TRACE": "dim blue",
+    "DEBUG": "cyan",
+    "INFO": "bold",
+    "SUCCESS": "bold green",
+    "WARNING": "yellow",
+    "ERROR": "bold red",
+    "CRITICAL": "bold white on red",
+}
+
+
+def _rich_logger_format(record: dict):
+    """Log message formatter"""
+    lvl_color = _color_map.get(record["level"].name, "cyan")
+    return (
+        "[not bold green]{time:YYYY/MM/DD HH:mm:ss}[/not bold green] | "
+        + f"[{lvl_color}]{{level.name}}[/{lvl_color}]"
+        + f"  - [{lvl_color}]{{message}}[/{lvl_color}]"
+    )
+
+
+class LoggerTableHandler(logging.Handler):
+    def __init__(
+        self,
+        log_table: Table,
+        layout: Layout,
+        log_level: str,
+        redraw_func: Callable[[list[str]], Table],
+    ):
+        super().__init__()
+        self.log_table = log_table
+        self.log_list = []
+        self.layout = layout
+        # self.log_format = _rich_logger_format
+        self.redraw_func = redraw_func
+        self.setLevel(log_level)
+        # Could set colors for levels here.
+
+    def emit(self, record):
+        msg = self.format(record)
+        tsize = os.get_terminal_size().lines // 2
+        if len(self.log_list) > tsize:
+            self.log_list.append(msg)
+            self.log_list.pop(0)
+            self.log_table = self.redraw_func(self.log_list)
+            self.layout.update(Panel(self.log_table, border_style="red"))
+        else:
+            self.log_table.add_row(msg)
+            self.log_list.append(msg)
+
+
+def set_rich_logger(
+    tabel: Table,
+    layout: Layout,
+    log_level: str,
+    redraw_func: Callable[[list[str]], Table],
+) -> Logger:
+    """设置一个可以在特定位置显示的logger
+
+    Args:
+        tabel (Table): 初始 log table
+        layout (Layout): log table 所在的 layout
+        log_level (str): log level
+        redraw_func (Callable[[list[str]], Table]): 重绘 log table 的方法
+    """
+    logger.remove()
+    logger.add(
+        LoggerTableHandler(tabel, layout, log_level, redraw_func),  # type: ignore
+        level=log_level,
+        enqueue=True,
+        format=_rich_logger_format,  # type: ignore
+    )
+    return logger  # type: ignore
+
+
+if __name__ == "__main__":
+    import random
+    from time import sleep
+
+    from rich.console import Console
+    from rich.live import Live
+    from rich.traceback import install
+
+    def _make_defalut_logger_table() -> Table:
+        """制作默认的logger table"""
+        table = Table(
+            expand=True,
+            show_header=False,
+            header_style="bold",
+            title="[blue][b]Log Messages[/b]",
+            highlight=True,
+        )
+        table.add_column("Log Output")
+        return table
+
+    def redraw_logger_table(log_list: list[str]) -> Table:
+        """重绘logger table"""
+        table = _make_defalut_logger_table()
+        for log in log_list:
+            table.add_row(log)
+        return table
+
+    console = Console()
+    install(console=console, show_locals=True)
+    table = _make_defalut_logger_table()
+    layout = Layout()
+    lf = Layout(Panel(table))
+    lr = Layout(Panel("right"))
+    layout.split_row(lf, lr)
+    with Live(layout, console=console, refresh_per_second=10, screen=True):
+        logger = set_rich_logger(table, lf, "INFO", redraw_logger_table)
+        for _ in range(100):
+            sleep(1)
+            log = random.choice(["INFO", "DEBUG", "WARNING", "ERROR", "CRITICAL"])
+            getattr(logger, log.lower())(f"this is a {log} log")
